7186d4af3eb4fb7508581912c6ca8b198912e2ef
CN74 <cn74@CN74s-MacBook-Pro.local>
2019-09-18 09:24:47 +0100

    Updates

---
+++ precache-manifest.b060abd55730d40992edaa4e764acefc.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "330ae706c997eebbe0ee692d5345a671",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "c091ae795e6993ff103b",
+    "url": "/portfolio/static/css/main.d5d896ff.chunk.css"
+  },
+  {
+    "revision": "c83d81b4c0a7531fc4b2",
+    "url": "/portfolio/static/js/2.e114199f.chunk.js"
+  },
+  {
+    "revision": "c091ae795e6993ff103b",
+    "url": "/portfolio/static/js/main.c1693dcd.chunk.js"
+  },
+  {
+    "revision": "35079273404e59a8285d",
+    "url": "/portfolio/static/js/runtime~main.6d0b08a1.js"
+  },
+  {
+    "revision": "fac9b054ec877c62552240feb43a244d",
+    "url": "/portfolio/static/media/affinityDesigner.fac9b054.png"
+  },
+  {
+    "revision": "802e21e629efecefdde5357dc194c0d7",
+    "url": "/portfolio/static/media/cPlusPlus.802e21e6.png"
+  },
+  {
+    "revision": "683a9ec262cd9d5767e0e9ef05183e75",
+    "url": "/portfolio/static/media/css.683a9ec2.png"
+  },
+  {
+    "revision": "c0ddb1142ae0c6e644681fe85319c632",
+    "url": "/portfolio/static/media/dividerBlue.c0ddb114.png"
+  },
+  {
+    "revision": "8fb4351617dffa0636b41072a8d11f8d",
+    "url": "/portfolio/static/media/dividerPink.8fb43516.png"
+  },
+  {
+    "revision": "a9919ab6b4edb647802162d8d2dd7caf",
+    "url": "/portfolio/static/media/git.a9919ab6.png"
+  },
+  {
+    "revision": "7187cacf051a3eb1f89e2b804da5a22e",
+    "url": "/portfolio/static/media/html.7187cacf.png"
+  },
+  {
+    "revision": "00a3c5cb2d8aa3aeb027777ae4563aae",
+    "url": "/portfolio/static/media/js.00a3c5cb.png"
+  },
+  {
+    "revision": "519e20457e7e3f3aba32da84bb130033",
+    "url": "/portfolio/static/media/linkedInLogo.519e2045.png"
+  },
+  {
+    "revision": "1abcba7eda65dc6cea69786423d7e5d8",
+    "url": "/portfolio/static/media/maya.1abcba7e.png"
+  },
+  {
+    "revision": "d85425441972a384b5bc87d7db210655",
+    "url": "/portfolio/static/media/node.d8542544.png"
+  },
+  {
+    "revision": "4df685b6442e6bbcfa77c7fba46fc4fe",
+    "url": "/portfolio/static/media/python.4df685b6.png"
+  },
+  {
+    "revision": "1d6a353e5ae3d3b76ce1585c7603bbfd",
+    "url": "/portfolio/static/media/random002.1d6a353e.jpg"
+  },
+  {
+    "revision": "3a0b5e4769f11a56c740a65473f79e97",
+    "url": "/portfolio/static/media/react.3a0b5e47.png"
+  }
+]);
