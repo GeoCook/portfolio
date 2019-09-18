0adf96f349958b773c94cb0b2622947f31debf22
CN74 <cn74@CN74s-MacBook-Pro.local>
2019-09-18 09:19:23 +0100

    Updates

---
+++ precache-manifest.faf040b61a3cd1b0393ccb1071e3a3b5.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "91c4db0e839ca549d966b9094ff8e14c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "413002a99a1a0236b1ca",
+    "url": "/static/css/main.d5d896ff.chunk.css"
+  },
+  {
+    "revision": "c83d81b4c0a7531fc4b2",
+    "url": "/static/js/2.e114199f.chunk.js"
+  },
+  {
+    "revision": "413002a99a1a0236b1ca",
+    "url": "/static/js/main.faba3bdc.chunk.js"
+  },
+  {
+    "revision": "d483b21b53fa8e5d19ff",
+    "url": "/static/js/runtime~main.0027d5f0.js"
+  },
+  {
+    "revision": "fac9b054ec877c62552240feb43a244d",
+    "url": "/static/media/affinityDesigner.fac9b054.png"
+  },
+  {
+    "revision": "802e21e629efecefdde5357dc194c0d7",
+    "url": "/static/media/cPlusPlus.802e21e6.png"
+  },
+  {
+    "revision": "683a9ec262cd9d5767e0e9ef05183e75",
+    "url": "/static/media/css.683a9ec2.png"
+  },
+  {
+    "revision": "c0ddb1142ae0c6e644681fe85319c632",
+    "url": "/static/media/dividerBlue.c0ddb114.png"
+  },
+  {
+    "revision": "8fb4351617dffa0636b41072a8d11f8d",
+    "url": "/static/media/dividerPink.8fb43516.png"
+  },
+  {
+    "revision": "a9919ab6b4edb647802162d8d2dd7caf",
+    "url": "/static/media/git.a9919ab6.png"
+  },
+  {
+    "revision": "7187cacf051a3eb1f89e2b804da5a22e",
+    "url": "/static/media/html.7187cacf.png"
+  },
+  {
+    "revision": "00a3c5cb2d8aa3aeb027777ae4563aae",
+    "url": "/static/media/js.00a3c5cb.png"
+  },
+  {
+    "revision": "519e20457e7e3f3aba32da84bb130033",
+    "url": "/static/media/linkedInLogo.519e2045.png"
+  },
+  {
+    "revision": "1abcba7eda65dc6cea69786423d7e5d8",
+    "url": "/static/media/maya.1abcba7e.png"
+  },
+  {
+    "revision": "d85425441972a384b5bc87d7db210655",
+    "url": "/static/media/node.d8542544.png"
+  },
+  {
+    "revision": "4df685b6442e6bbcfa77c7fba46fc4fe",
+    "url": "/static/media/python.4df685b6.png"
+  },
+  {
+    "revision": "1d6a353e5ae3d3b76ce1585c7603bbfd",
+    "url": "/static/media/random002.1d6a353e.jpg"
+  },
+  {
+    "revision": "3a0b5e4769f11a56c740a65473f79e97",
+    "url": "/static/media/react.3a0b5e47.png"
+  }
+]);
